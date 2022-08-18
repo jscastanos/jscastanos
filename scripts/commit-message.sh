4cb5d2412ba593a79667e1ad503139902ba64d12
Jec Castanos <jec.castanos@gmail.com>
2022-08-19 07:40:14 +0800

    script: remove {{

--- scripts/commit-message.sh
+++ scripts/commit-message.sh
@@ -2,13 +2,13 @@
 
 if [ $SCHEDULE -eq '0 21 * * *' ]
 then 
-  echo "COMMIT_MESSAGE={Wake Up Owl}" >> $GITHUB_ENV
+  echo "COMMIT_MESSAGE=Wake Up Owl" >> $GITHUB_ENV
 elif [ $SCHEDULE -eq '0 4 * * *' ]
 then 
-  echo "COMMIT_MESSAGE={Tired Owl}" >> $GITHUB_ENV
+  echo "COMMIT_MESSAGE=Tired Owl" >> $GITHUB_ENV
 elif [ $SCHEDULE -eq '0 12 * * *' ]
 then 
-  echo "COMMIT_MESSAGE={Sleeping Owl}" >> $GITHUB_ENV
+  echo "COMMIT_MESSAGE=Sleeping Owl" >> $GITHUB_ENV
 else
-  echo "COMMIT_MESSAGE={Owl Owl}" >> $GITHUB_ENV
+  echo "COMMIT_MESSAGE=Owl Owl" >> $GITHUB_ENV
 fi

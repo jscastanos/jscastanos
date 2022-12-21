d803a769f5e40543ffc7b423c4f5450873fabd71
Jec Castaños <37854603+jscastanos@users.noreply.github.com>
2022-12-21 15:10:17 +0800

    script: fix commit message

--- scripts/commit-message.sh
+++ scripts/commit-message.sh
@@ -3,11 +3,11 @@
 # print what we got
 echo "Current schedule is $SCHEDULE" 
 
-if [ "$SCHEDULE" == "0 21 * * *" ]; then 
+if [ "$SCHEDULE" == "0 21 * * 1-5" ]; then 
   echo "COMMIT_MESSAGE=Wake Up Owl" >> $GITHUB_ENV
-elif [ "$SCHEDULE" == "0 4 * * *" ]; then 
+elif [ "$SCHEDULE" == "0 4 * * 1-5" ]; then 
   echo "COMMIT_MESSAGE=Tired Owl" >> $GITHUB_ENV
-elif [ "$SCHEDULE" == "0 15 * * *" ]; then 
+elif [ "$SCHEDULE" == "0 15 * * 1-5" ]; then 
   echo "COMMIT_MESSAGE=Sleeping Owl" >> $GITHUB_ENV
 else
   echo "COMMIT_MESSAGE=Owl Owl" >> $GITHUB_ENV
